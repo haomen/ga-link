a3879820cc08e1aa49e15212e42d84588bca94cb
Hao Men <hao.men@gmail.com>
2021-12-27 19:53:11 -0500

    Minor update about names

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 function redirect(request) {
-  const prefix = 'http://go/';
+  const prefix = 'http://ga/';
   const path = request.url.slice(prefix.length);
-  console.log("redirecting to go link!");
+  // console.log("redirecting to ga link!");
   return { redirectUrl: 'http://192.168.1.3:8080/redirect/' + path };
 }
 
